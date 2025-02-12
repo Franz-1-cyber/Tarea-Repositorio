f287f9e3c5076d7569ca3cc6a499184ddaea24fd
Franz-1-cyber <infernalios666@gmail.com>
2025-02-12 15:43:23 -0400

    Subiendo archivos al nuevo repositorio

---
+++ Leccion_5.py
@@ -0,0 +1,8 @@
+# Solicitar una cadena al usuario
+cadena = input("Ingresa una palabra o frase: ")
+
+# Usar len() para obtener la longitud de la cadena
+longitud = len(cadena)
+
+# Mostrar la longitud
+print("La longitud de la cadena es:", longitud)
